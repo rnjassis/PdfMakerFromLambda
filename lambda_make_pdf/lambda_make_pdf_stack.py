67289bc11902d6cae401c972946657f4ed6e2a60
Rafael Silva <rafaelsilvarnj@gmail.com>
2023-03-30 14:17:30 -0300

    Dependency handling - Added another way to handle dependency: aws_lambda_python_alpha - Remade the src/layer/ to reflect the correct hierarchy for the layer   in python

--- lambda_make_pdf/lambda_make_pdf_stack.py
+++ lambda_make_pdf/lambda_make_pdf_stack.py
@@ -3,7 +3,8 @@ from aws_cdk import (
     Duration,
     aws_s3 as s3,
     aws_lambda as lambda_,
-    aws_iam as iam
+    aws_iam as iam,
+    aws_lambda_python_alpha as alambda_
 )
 from constructs import Construct
 
@@ -32,15 +33,28 @@ class LambdaMakePdfStack(Stack):
                                            "lambda_layer",
                                            code = lambda_.AssetCode("./src/layer/"),
                                            compatible_runtimes = [lambda_.Runtime.PYTHON_3_8])
-        # Lambda
+#        # Lambda
         lambdaFunction = lambda_.Function(self,
                                           "CreatePdfFunction",
                                           runtime=lambda_.Runtime.PYTHON_3_8,
-                                          code=lambda_.Code.from_asset('src/code/'),
+                                          code=lambda_.Code.from_asset('./src/code/'),
                                           handler='Main.handler',
                                           initial_policy=[s3ReadWrite, s3ListObjects], 
                                           timeout=Duration.seconds(180),
                                           function_name="CreatePdfFunctionCDK",
                                           layers = [lambdaLayer],
                                           environment={'iPath':'supplier/images/','oPath':'supplier/pdf/' ,'BUCKET_NAME': bucket.bucket_name})
+
+        ##### SOLUTION 2  - **EXPERIMENTAL** AWS_LAMBDA_PYTHON_ALPHA
+#        initiator = alambda_.PythonFunction(
+#                self,
+#                'CreatePdfFunction',
+#                entry = './src/code/',
+#                runtime = lambda_.Runtime.PYTHON_3_8,
+#                index='Main.py',
+#                handler='handler',
+#                initial_policy=[s3ReadWrite, s3ListObjects], 
+#                timeout=Duration.seconds(180),
+#                environment={'iPath':'supplier/images/','oPath':'supplier/pdf/' ,'BUCKET_NAME': bucket.bucket_name}
+#                )
 
